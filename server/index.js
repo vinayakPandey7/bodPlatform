280b135532891606aacc5138fe4193d79a86adeb
pwvinayakpandey <vinayak.pandey+1@pw.live>
2025-07-26 20:02:49 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -30,8 +30,7 @@ app.use(
       "http://localhost:3001",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:3001",
-      "https://bod-platform.vercel.app",
-      
+      "https://ciero.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
